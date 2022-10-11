7ffd72ad970d26bf8f11c02761a5b529fb24c7dd
viviannemelo <viviannemmelo@gmail.com>
2022-10-11 17:04:09 -0300

    Requisito 03: testes fetchItem

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,19 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  
-  fail('Teste vazio');
+  it('Teste se fetchItem é uma função', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+  it('Execute a função fetchItem com o argumento do item "MLB1615760527" e teste se fetch foi chamada', () => {
+    expect(fetchItem('MLB1615760527')).toHaveBeenCalled;
+  });
+  it('Teste se, ao chamar a função fetchItem com o argumento do item "MLB1615760527", a função fetch utiliza o endpoint "https://api.mercadolibre.com/items/MLB1615760527"', async () => {
+    //
+  })
+  it('Teste se o retorno da função fetchItem com o argumento do item "MLB1615760527" é uma estrutura de dados igual ao objeto item que já está importado no arquivo.', () => {
+    expect(fetchItem('MLB1615760527')).toEqual(item.body);
+  })
+  it('', () => {
+    expect(fetchItem()).toThrow;
+  })
 });

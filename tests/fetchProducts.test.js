5ff70cff7974e293bd3367d4e61d8b3a841908bf
viviannemelo <viviannemmelo@gmail.com>
2022-10-11 16:05:14 -0300

    Requisito 01: incompleto, porém testes passados.

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,8 +1,22 @@
 require('../mocks/fetchSimulator');
 const { fetchProducts } = require('../helpers/fetchProducts');
+const fetchSimulator = require('../mocks/fetchSimulator');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  })
+  it('Execute a função fetchProducts com o argumento computador e teste se fetch foi chamada', async () => {
+    expect(fetchProducts('computador')).toHaveBeenCalled;
+  })
+  it('Teste se, ao chamar a função fetchProducts com o argumento computador, a função fetch utiliza o endpoint https://api.mercadolibre.com/sites/MLB/search?q=computador', () => {
+    //
+  })
+  it('Teste se o retorno da função fetchProducts com o argumento computador é uma estrutura de dados igual ao objeto computadorSearch', () => {
+    expect(fetchProducts('computador')).toStrictEqual(computadorSearch.body);
+  });
+  it('', () => {
+    expect(fetchProducts()).toThrow;
+  });
 });

fd5eeae0e00f8e25a30330d1c87f5e757d191270
viviannemelo <viviannemmelo@gmail.com>
2022-10-17 10:17:44 -0300

    Requisito 05: correção do lint

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ const createProductItemElement = ({ id, title, thumbnail }) => {
  * @param {Element} product - Elemento do produto.
  * @returns {string} ID do produto.
  */
-const getIdFromProductItem = (product) => product.querySelector('span.id').innerText;
+// const getIdFromProductItem = (product) => product.querySelector('span.id').innerText;
 
 /**
  * Função responsável por criar e retornar um item do carrinho.
